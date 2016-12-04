2891c70949f0d326a93c8e4e029aa664977c618f
Steve Kinney <hello@stevekinney.net>
2016-12-03 20:02:34 -0700

    Finish up first pass at completed example

--- app/renderer.js
+++ app/renderer.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { remote, shell, clipboard } = require('electron');
+const currentWindow = remote.getCurrentWindow();
+
+let currentFile = null;
+let originalContent = '';
+
 const getTextStatistics = require('./lib/generate-statistics');
 
 const content = document.getElementById('content');
@@ -8,11 +16,67 @@ const openFile = document.getElementById('open-file');
 const saveFile = document.getElementById('save-file');
 const copyToClipboard = document.getElementById('copy-to-clipboard');
 
-const renderStatistics = () => {
-  const { lines, words, text } = getTextStatistics(content.value);
+const updateUserInterface = (content) => {
+  const { lines, words, text } = getTextStatistics(content);
   wordCount.textContent = words;
   lineCount.textContent = lines;
   readingTime.textContent = text;
+
+  currentWindow.setDocumentEdited(content !== originalContent);
 };
 
-content.addEventListener('keyup', renderStatistics);
+const updateCurrentFile = (file, text) => {
+  currentFile = file;
+  content.value = text;
+  originalContent = text;
+
+  remote.app.addRecentDocument(file);
+
+  currentWindow.setTitle(file);
+  currentWindow.setRepresentedFilename(file);
+
+  updateUserInterface(text);
+};
+
+content.addEventListener('keyup', () => {
+  updateUserInterface(content.value);
+});
+
+openFile.addEventListener('click', () => {
+  const files = remote.dialog.showOpenDialog(currentWindow, {
+    title: 'Open File',
+    properties: ['openFile'],
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'text'] },
+      { name: 'Markdown', extensions: ['markdown', 'md'] }
+    ]
+  });
+
+  if (!files) { return; }
+
+  const file = files[0];
+  const text = fs.readFileSync(file).toString();
+
+  updateCurrentFile(file, text);
+});
+
+saveFile.addEventListener('click', () => {
+  const file = currentFile || remote.dialog.showSaveDialog(currentWindow, {
+    title: 'Save File',
+    defaultPath: remote.app.getPath('documents'),
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'text'] },
+      { name: 'Markdown', extensions: ['markdown', 'md'] }
+    ]
+  });
+
+  if (!file) { return; }
+
+  fs.writeFileSync(file, content.value);
+  updateCurrentFile(file, content.value);
+  shell.showItemInFolder(file);
+});
+
+copyToClipboard.addEventListener('click', () => {
+  clipboard.writeText(content.value);
+});
